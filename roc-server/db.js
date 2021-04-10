07d2cecdf63d6d7a5360a799fa17d328b65e95f1
David Rosenberg <dmr@davidrosenberg.me>
2021-04-10 15:31:31 -0400

    parameterized json queries

--- roc-server/db.js
+++ roc-server/db.js
@@ -25,9 +25,41 @@ db.getRegion = async function (regionId) {
   return(rows[0])
 }
 
-db.getAllRegions = async function() {
-  const { rows } = await this.query(`
-    SELECT
+db.searchRegions = async function(
+  target = null,
+  fractionation = null,
+  intent = null,
+  importance = null
+) {
+  var whereClause = 'WHERE region.id > 0 '
+  if (target !== null) {
+    var tgts = target.split(',')
+    whereClause += `AND target.name in ( ${"'" + tgts.join("', '") + "'"} ) `
+  }
+  if (fractionation !== null) {
+    if (fractionation.match(/^[\d|-|,]*$/)) {
+      // is a number, list of numbers, or numeric range
+      if (fractionation.match(/^\d*-\d*$/)) {
+        [fmin, fmax] = fractionation.split('-')
+        whereClause += `AND fractions_min >= ${fmin} and (fractions_max <= ${fmax} OR fractions_max is null) `
+      } else {
+        whereClause += `AND fractions_min in (${fractionation}) `
+      }
+    } else {
+      var fracs = fractionation.split(',')
+      whereClause += `AND fractionation.description in ( ${"'" + fracs.join("', '") + "'"} ) `
+    }
+  }
+  if (intent !== null) {
+    var intents = intent.split(',')
+    whereClause += ` AND intent.description in ( ${"'" + intents.join("', '") + "'"} ) `
+  }
+  if (importance !== null) {
+    var imps = importantce.split(',')
+    whereClause += ` AND region.importance in ( ${"'" + imps.join("', '") + "'"} ) `
+  }
+  const query = `
+  SELECT
       region.id,
       target.name,
       coalesce(fractionation.description, '') || coalesce(fractions_min::text, '') || coalesce('-' || fractions_max::text, '') as fractionation,
@@ -47,10 +79,44 @@ db.getAllRegions = async function() {
       LEFT JOIN intent on region.intent = intent.id
       LEFT JOIN volume_type vtype2 on region.volume_deviation_type = vtype2.id
       LEFT JOIN dose_type dtype on region.dose_type = dtype.id
-      LEFT JOIN dose_type dtype2 on region.dose_deviation_type = dtype.id;`, []
-  )
+      LEFT JOIN dose_type dtype2 on region.dose_deviation_type = dtype.id 
+    ${whereClause};`
+  const { rows } = await this.query(query, [])
+  return(rows);
+}
+
+db.getAllRegions = async function() {
+  const rows = await db.searchRegions()
   return(rows)
 }
+
+
+
+  // this.query(`
+  //   SELECT
+  //     region.id,
+  //     target.name,
+  //     coalesce(fractionation.description, '') || coalesce(fractions_min::text, '') || coalesce('-' || fractions_max::text, '') as fractionation,
+  //     intent.description,
+  //     volume::float / 100 || ' ' || vtype.description as volume,
+  //     volume_deviation::float / 100 || ' ' || vtype2.description as volume_deviation,
+  //     prv,
+  //     dose::float / 100 || ' ' || dtype.description as dose,
+  //     dose_deviation::float / 100 || ' ' || dtype2.description as dose_deviation,
+  //     conversion,
+  //     importance
+  //   FROM
+  //     region
+  //     LEFT JOIN target on region.target = target.id
+  //     LEFT JOIN fractionation on region.fractionation = fractionation.id
+  //     LEFT JOIN volume_type vtype on region.volume_type = vtype.id
+  //     LEFT JOIN intent on region.intent = intent.id
+  //     LEFT JOIN volume_type vtype2 on region.volume_deviation_type = vtype2.id
+  //     LEFT JOIN dose_type dtype on region.dose_type = dtype.id
+  //     LEFT JOIN dose_type dtype2 on region.dose_deviation_type = dtype.id;`, []
+  // )
+  // return(rows)
+// }
 
 
 module.exports = db
--- roc-server/routes.js
+++ roc-server/routes.js
@@ -30,5 +30,17 @@ router.get('/allregions', async function(req, res) {
   res.json(regions)
 })
 
+router.get('/search', async function(req, res) {
+  console.log(req.body)
+  const target = req.body.target || null
+  const fractionation = req.body.fractionation || null
+  const intent = req.body.intent || null
+  const importance = req.body.importance || null
+
+  const regions = await db.searchRegions(target, fractionation, intent, importance)
+  res.json(regions)
+})
+
+
 module.exports = router
 
